d51d1bb058fefe6fbd60048f14c0abd2e9842ecb
Mr H <u20201b742@upc.edu.pe>
2025-09-01 18:08:06 -0500

    Backend

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -1,11 +1,11 @@
 const admin = require('firebase-admin');
  
-//const serviceAccount = require('../../firebase-key.json'); 
-const serviceAccount = require('/etc/secrets/firebase-key.json');
+const serviceAccount = require('../../firebase-key.json'); 
+//const serviceAccount = require('/etc/secrets/firebase-key.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  storageBucket: 'gs://easy-park-pe.firebasestorage.app'
+  storageBucket: 'gs://secupro-28b31.firebasestorage.app'
 });
 
 const bucket = admin.storage().bucket();
